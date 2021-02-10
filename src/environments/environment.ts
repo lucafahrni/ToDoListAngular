37260b26b3695f9902f5fb4d0c49d571ef3cc1b4
Luca <lucafahrni@yahoo.com>
2021-02-10 09:49:45 +0100

    Added Project

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+
+
+export const environment = {
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyCHYdy88bwCn_9NvuOxC2NCULSKoy-xEvc",
+    authDomain: "todolistapp-8361f.firebaseapp.com",
+    databaseURL: "https://todolistapp-8361f.firebaseio.com",
+    projectId: "todolistapp-8361f",
+    storageBucket: "todolistapp-8361f.appspot.com",
+    messagingSenderId: "954824914202"
+  }
+};
